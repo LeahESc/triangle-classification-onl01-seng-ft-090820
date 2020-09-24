c2a6e08e8971b84cc61b48e85f90def70e7b2b2b
Leah Schlackman <leahemily1@gmail.com>
2020-09-24 18:48:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,14 +2,16 @@ class Triangle
   attr_accessor :x, :y, :z, :equilateral, :isosceles, :scalene, :sides 
   
   def initialize(x, y, z)
+    @x = x 
+    @y = y 
+    @z = z
     @sides = [x,y,z]
   end 
   
   def kind
     # @sides.sort 
-    if 
-      # @sides[0] + @sides[1] <= @sides[2] || @sides.any? {|side| side <= 0}
-      x + y < z || z + y < x || x + z < y || x <= 0 || x == nil || y <=  0 || y == nil || z <=  0 || z == nil 
+    # @sides[0] + @sides[1] <= @sides[2] || @sides.any? {|side| side <= 0}
+    if   x + y < z || z + y < x || x + z < y || x <= 0 || x == nil || y <=  0 || y == nil || z <=  0 || z == nil 
         raise TriangleError
      elsif x == y && y == z && x == z
       :equilateral
